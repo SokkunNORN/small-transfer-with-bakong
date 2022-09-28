b5b6eaaddf3ab2074cf3c0893fff326603279600
sokkun <sokkun.norn2019@gmail.com>
2022-09-28 16:53:26 +0700

    feat(database): add table qr code

---
+++ src/main/kotlin/me/transfer/transferbakongapi/model/CurrencyType.kt
@@ -0,0 +1,16 @@
+package me.transfer.transferbakongapi.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "currency_type")
+data class CurrencyType(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(name = "name", nullable = false)
+    val name: String,
+
+    @Column(name = "code", nullable = false)
+    val code: String
+)
